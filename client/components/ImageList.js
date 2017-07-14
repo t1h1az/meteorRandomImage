74de5b341d8e1b51b5f9b294a4aff48d42f52f26
Thomas Beetz <beetz.thomas@googlemail.com>
2017-07-14 22:36:56 +0200

    added axios http request from imgur api

--- client/components/ImageList.js
+++ client/components/ImageList.js
@@ -1,25 +1,25 @@
 import React, {Component} from 'react';
 import ImageDetail from './ImageDetail';
+import axios from 'axios';
 
-const IMAGES = [
+const IMAGESA = [
   { title: 'this', link: 'http://dummyimage.com/100x80', description: 'afaefu a elkfjh akfjh ekjh  kjhaefka jekjl'},
   { title: 'what do you want', link: 'http://dummyimage.com/100x80', description: 'afaefu a elkfjh akfjh ekjh  kjhaefka jekjl'},
   { title: 'more pictures', link: 'http://dummyimage.com/100x80', description: 'afaefu a elkfjh akfjh ekjh  kjhaefka jekjl'},
   { title: 'no no no more', link: 'http://dummyimage.com/100x80', description: 'afaefu a elkfjh akfjh ekjh  kjhaefka jekjl'}
 ];
 
-const ImageList = () => {
-  const RenderedImages = IMAGES.map((image) => {
+const ImageList = (props) => {
+  const RenderedImagesA = IMAGESA.map((image) => {
+    console.log(props.images);
     return <ImageDetail image={image} key={image.title}/>;
-  
   });
-  
+
   return (
     <ul className="media-list list-group">
-      {RenderedImages}
+      {RenderedImagesA}
     </ul>
   );
 };
 
 export default ImageList;
-
--- client/main.js
+++ client/main.js
@@ -1,11 +1,30 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import ImageList from './components/ImageList';
+import axios from 'axios';
 
-const App = () => {
-  return <ImageList />;
-};
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {images: []};
+  }
+  componentWillMount() {
+    //Load data here mothafucka, no need for done then
+      axios.get('https://api.imgur.com/3/gallery/hot/viral/0')
+        .then((response) => this.setState({images: response.data.data}));
+  }
+
+  render() {
+    console.log(this.state.images);
+    return <ImageList />;
+  }
+}
 
 Meteor.startup(() => {
   ReactDOM.render(<App />, document.querySelector('.container'));
+  axios.get('https://api.imgur.com/3/gallery/hot/viral/0')
+    .then(response => console.log(response));
 });
